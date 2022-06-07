f807fb0df7aba16b754db5b49c469eb6e5bffb5b
ducmanh1666 <manhlvdse130605@fpt.edu.vn>
2022-06-08 01:48:09 +0700

    First commit

---
+++ lib/screens/welcome.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+//
+// import 'home.dart';
+//
+// class Welcome extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       body: Stack(
+//         children: <Widget>[
+//           Image.asset(
+//             'assets/images/lg.png'
+//             fit: BoxFit.fill,
+//             height: double.infinity,
+//             width: double.infinity,
+//           )
+//         ],
+//       ),
+//     )
+//   }
+// }
