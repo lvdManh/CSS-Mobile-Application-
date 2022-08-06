6387bcf76b5c358a771ac0076f0a25332cbea3ba
hungndse1306702 <hungndse130670@gmail.com>
2022-08-07 04:37:38 +0700

    con thieu accessory details

--- lib/screens/widgets/product_widget.dart
+++ lib/screens/widgets/product_widget.dart
@@ -3,32 +3,53 @@ import 'package:flutter/material.dart';
 import '../../models/product.dart';
 
 class ProductWidget extends StatelessWidget {
-  final Product product;
+  final Accessory? product;
 
-  const ProductWidget({Key? key, required this.product})
-      : assert(product != null),
-        super(key: key);
+  const ProductWidget({Key? key, required this.product}) : super(key: key);
+
+  String convertMoney(int? price) {
+    // final value = "XPTOXXSFXBAC"
+    //     .replaceAllMapped(RegExp(r".{4}"), (match) => "${match.group(0)} ");
+    // print("value: $value");
+    String converted = price.toString().replaceAllMapped(
+        RegExp(r"(?<=\d)(?=(\d\d\d)+(?!\d))"), (match) => "${match.group(0)}.");
+    return converted;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        onTap: (){
-          print("${product.name} pressed");
+        onTap: () {
+          print("${product?.name} pressed");
         },
-        leading: Image.network(product.imgUrl),
-        title: Text(product.name,
+        leading: Image(
+          image: AssetImage(checkAccessoryName()),
+          width: 70,
         ),
-        subtitle: Text(product.description),
-        trailing: Text(product.price.toString(),
+        title: Text('${product?.name}'),
+        subtitle: Text('${product?.description}'),
+        trailing: Text(
+          convertMoney(product?.price),
           textScaleFactor: 1.1,
           style: TextStyle(
             color: Colors.black54,
             fontWeight: FontWeight.bold,
-          ),),
+          ),
+        ),
       ),
     );
   }
+
+  String checkAccessoryName() {
+    if (product?.name == 'RAM') {
+      return 'assets/images/ram.png';
+    } else if (product?.name == 'CPU') {
+      return 'assets/images/cpu.png';
+    } else if (product?.name == 'Mainboard') {
+      return 'assets/images/mainboard.png';
+    } else {
+      return 'assets/images/white.png';
+    }
+  }
 }
-
-
